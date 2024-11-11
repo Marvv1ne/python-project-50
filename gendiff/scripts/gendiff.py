61cb7d2536ad07632bdc029588dec3f33e69b533
Marvv1ne <gorn89@inbox.ru>
2024-11-11 22:02:32 +0400

    improve generate_diff function and add print_diff function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,11 @@
 from pprint import pprint
 from .cli import args_parser
-from .parser import generate_diff
+from .parser import generate_diff, print_diff
 
 def main():
     file1, file2 = args_parser()
-    pprint(generate_diff(file1, file2))
+    difference = generate_diff(file1, file2)
+    print_diff(difference)
     
 
 if __name__ == "__main__":
--- gendiff/scripts/parser.py
+++ gendiff/scripts/parser.py
@@ -1,12 +1,28 @@
 import os
 import json
+from collections import ChainMap
 
-def file_reader(file_name):
+def json_reader(file_name):
     CURRENT_DIR = os.getcwd()
     return json.load(open(f'{CURRENT_DIR}/{file_name}'))
-    
+
 def generate_diff(first_file, second_file):
-    file_1 = file_reader(first_file)
-    file_2 = file_reader(second_file)
-    return file_1, file_2
+    file_1 = json_reader(first_file)
+    file_2 = json_reader(second_file)
+    file_d = ChainMap(file_1, file_2)
+    result = '\n'
+    for key in sorted(file_d.keys()):
+        if key not in file_2:
+            result += f"- {key}: {file_1[key]}\n"
+        elif key not in file_1:
+            result += f"+ {key}: {file_2[key]}\n"
+        elif file_1[key] != file_2[key]:
+            result += f"- {key}: {file_1[key]}\n"
+            result += f"- {key}: {file_2[key]}\n"
+        else:
+            result += f"  {key}: {file_1[key]}\n"
+    return result
+
+def print_diff(difference):
     
+    print('{', difference, '}')
