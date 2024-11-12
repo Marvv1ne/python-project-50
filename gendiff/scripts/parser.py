abedf6863f69e7d5279c541a739a7237e8b3a737
Gura-san <gorn89@inbox.ru>
2024-11-12 19:04:11 +0400

    intermediate commit

--- gendiff/scripts/parser.py
+++ gendiff/scripts/parser.py
@@ -6,21 +6,24 @@ def json_reader(file_name):
     CURRENT_DIR = os.getcwd()
     return json.load(open(f'{CURRENT_DIR}/{file_name}'))
 
+def check_difference(key, dict_1, dict_2):
+    if key not in dict_2:
+        return f"- {key}: {dict_1[key]}\n"
+    elif key not in dict_1:
+        return f"+ {key}: {dict_2[key]}\n"
+    elif dict_1[key] != dict_2[key]:
+        return f"- {key}: {dict_1[key]}\n+ {key}: {dict_2[key]}\n"
+    else:
+        return f"  {key}: {dict_1[key]}\n"
+
 def generate_diff(first_file, second_file):
     file_1 = json_reader(first_file)
     file_2 = json_reader(second_file)
-    file_d = ChainMap(file_1, file_2)
-    result = '\n'
-    for key in sorted(file_d.keys()):
-        if key not in file_2:
-            result += f"- {key}: {file_1[key]}\n"
-        elif key not in file_1:
-            result += f"+ {key}: {file_2[key]}\n"
-        elif file_1[key] != file_2[key]:
-            result += f"- {key}: {file_1[key]}\n"
-            result += f"- {key}: {file_2[key]}\n"
-        else:
-            result += f"  {key}: {file_1[key]}\n"
+    all_keys = ChainMap(file_1, file_2).keys()
+    result = '{\n'
+    for key in sorted(all_keys):
+        result += check_difference(key, file_1, file_2)
+    result += '}'
     return result
 
 def print_diff(difference):
