dbb4e76a8e4349a73bfeecc12baafc22aac79607
Marvv1ne <gorn89@inbox.ru>
2024-11-19 21:49:37 +0400

    almost add fromater for new ast in tree.py

--- gendiff/scripts/formater.py
+++ gendiff/scripts/formater.py
@@ -1,25 +1,38 @@
 def stylish(tree, depth=1):
     result = []
-    space = '..'
+    spc = '..'
     for key, value in tree.items():
-        if value.get("status", None) == "updated":
-            result.extend([f"{space*depth}- {key}: {dict_formater(value["old_value"], space, depth+2)}\n",
-                               f"{space*depth}+ {key}: {dict_formater(value["old_value"], space, depth+2)}\n"])
-        elif value.get("status", None) == "deleted":
-            result.append(f"{space*depth}- {key}: {dict_formater(value["old_value"], space, depth+2)}\n")
-        elif value.get("status", None) == "inserted":
-            result.append(f"{space*depth}+ {key}: {dict_formater(value["new_value"], space, depth+2)}\n")
-        elif value.get("status", None) == "untouched":
-            result.append(f"{space*depth}  {key}: {dict_formater(value["old_value"], space, depth+2)}\n")
-        else:
-            result.append(f"{space*depth}  {key}: \n{stylish(value, depth+2)}")
+        if not isinstance(value, dict):
+            result.append(f"  {key}: {dict_formater(value, spc, depth)}")
+        elif value.get("type", None) == "branch":
+            result.append(f"{spc*depth}  {key}: {{\n")
+            result.append(f"{stylish(value['children'], depth+2)}\n")
+            result.append(f"{spc*depth}}}\n")
+        elif value.get("type", None) == "leaf":
+            result.append(f"{leaf_formater(key, value, spc, depth)}")
     return ''.join(result)
 
 
-def dict_formater(tree, space, depth):
-    result = []
-    if not isinstance(tree, dict):
-        return tree
-    for k, v in tree.items():
-        result.append(f"\n{space*(depth)}  {k}: {dict_formater(v, space, depth+2)}")
-    return ''.join(result)        
+def leaf_formater(key, leaf, spc, depth):
+    res = []
+    if leaf["status"] == "untouched":        
+        res.append(f"{spc*depth}  {key}: {dict_formater(leaf['old_value'], spc, depth+2)}\n")
+    elif leaf["status"] == "updated":        
+        res.append(f"{spc*depth}- {key}: {dict_formater(leaf['old_value'], spc, depth+2)}\n")
+        res.append(f"{spc*depth}+ {key}: {dict_formater(leaf['new_value'], spc, depth+2)}\n")
+    elif leaf["status"] == "deleted":        
+        res.append(f"{spc*depth}- {key}: {dict_formater(leaf['old_value'], spc, depth+2)}\n")
+    elif leaf["status"] == "inserted":                
+        res.append(f"{spc*depth}+ {key}: {dict_formater(leaf['new_value'], spc, depth+2)}\n")
+    
+    return ''.join(res)
+        
+
+def dict_formater(data, spc, depth):
+    if not isinstance(data, dict):
+        return data
+    res = ['{']
+    for k, v in data.items():
+        res.append(f"\n{spc*depth}  {k}: {dict_formater(v, spc, depth+2)}\n")
+    res.append(f"{spc*(depth-1)}}}")
+    return ''.join(res)
