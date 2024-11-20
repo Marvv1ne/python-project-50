ddcfc6c4c265f1bfa28b04b104487dee08033666
Gura-san <gorn89@inbox.ru>
2024-11-20 19:46:24 +0400

    insert format check into generate_diff function

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,17 +3,24 @@ from .cli import args_parser
 from .tree import make_tree
 from .parser import file_reader
 from pprint import pprint
-from .formater import stylish
+from ..formaters.stylish_formater import stylish
+from ..formaters.plane_formater import plane
 
-def generate_diff(first_file_name, second_file_name):
+
+def generate_diff(first_file_name, second_file_name, format):
     old_dict = file_reader(first_file_name)
     new_dict = file_reader(second_file_name)
-    return make_tree(old_dict, new_dict)
+    tree = make_tree(old_dict, new_dict)
+    match format:
+        case "stylish":
+            return stylish(tree)
+        case "plane":
+            return plane(tree)
+    
 
 def main():
-    first_file, second_file = args_parser()
-    diff = generate_diff(first_file, second_file)
-    print(stylish(diff))   
+    first_file, second_file, format = args_parser()
+    print(generate_diff(first_file, second_file, format))   
     
 
 if __name__ == "__main__":
