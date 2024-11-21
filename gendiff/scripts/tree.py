2c2d92114834e890aba93fda629f152e54c499a4
Gura-san <gorn89@inbox.ru>
2024-11-21 20:05:28 +0400

    add geters functions into tree.py

--- gendiff/scripts/tree.py
+++ gendiff/scripts/tree.py
@@ -22,29 +22,38 @@ def make_inserted_leaf(new_value):
 def make_untouched_leaf(value):
     return mkleaf(old_value=value, new_value=value, status="untouched")
 
+def get_value(key, some_dict):
+    return some_dict.get(key, None)
+
+def get_old_value(some_dict):
+    return some_dict.get("old_value", None)
+
+def get_new_value(some_dict):
+    return some_dict.get("new_value", None)
+
+def get_children(some_dict):
+    return some_dict.get("children", None)
 
 def check_leaf(key, old_dict, new_dict):
     diff_old_new = old_dict.keys() - new_dict.keys()
     diff_new_old = new_dict.keys() - old_dict.keys()
     intersection = old_dict.keys() & new_dict.keys()
+    old_value = get_value(key, old_dict)
+    new_value = get_value(key, new_dict)
     if key in diff_old_new:
-        old_value = old_dict[key]
         return {key: make_deleted_leaf(old_value)}
     elif key in diff_new_old:
-        new_value = new_dict[key]
         return {key: make_inserted_leaf(new_value)}
-    elif key in intersection:
-        old_value = old_dict[key]
-        new_value = new_dict[key]
-        return {key: make_updated_leaf(old_value, new_value)} if old_value != new_value else {key: make_untouched_leaf(old_value)}
-    else:
-        return "I dont know what to do sir!"
+    elif key in intersection and old_value != new_value:       
+        return {key: make_updated_leaf(old_value, new_value)}  
+    elif key in intersection and old_value == new_value:
+        return {key: make_untouched_leaf(old_value)}
 
 def is_branch(key, old_dict, new_dict):
     if type(old_dict) == str or type(new_dict) == str:
         return False
-    old_value = old_dict.get(key, None)
-    new_value = new_dict.get(key, None)
+    old_value = get_value(key, old_dict)
+    new_value = get_value(key, new_dict)
     return isinstance(old_value, dict) and isinstance(new_value, dict)
 
 def make_tree(old_dict, new_dict):
@@ -55,7 +64,7 @@ def make_tree(old_dict, new_dict):
         if not is_branch(key, old_dict, new_dict):
             tree.update(check_leaf(key, old_dict, new_dict))
         else:
-            old_value = old_dict.get(key, dict())
-            new_value = new_dict.get(key, dict())
-            tree.update({key: mkbranch(make_tree(old_value, new_value))})
+            o_dict = old_dict.get(key, dict())
+            n_dict = new_dict.get(key, dict())
+            tree.update({key: mkbranch(make_tree(o_dict, n_dict))})
     return tree
