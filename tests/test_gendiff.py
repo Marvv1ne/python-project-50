04f40dbdfe0b00c6eb31953dd07dfb377e1d8bc1
Marvv1ne <gorn89@inbox.ru>
2024-11-21 22:02:00 +0400

    add some tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,16 +2,24 @@ import pytest
 import os
 from gendiff.scripts.gendiff import generate_diff
 
-data_1 = os.path.join(os.path.dirname(__file__), 'fixtures', 'file1.json')
-data_2 = os.path.join(os.path.dirname(__file__), 'fixtures', 'file2.json')
+data_3_json = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file3.json')
+data_4_json = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file4.json')
+data_3_yml = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file3.yml')
+data_4_yml = os.path.join(os.getcwd(), 'tests', 'fixtures', 'file4.yml')
 
-def test_gendiff():
-    correct = """{
-- follow: False
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: True
-}"""
-    assert generate_diff(data_1, data_2) == correct
+correct_stylish_data_3 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'stylish_result_3_4.txt')
+correct_plane_data_3 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'plane_result_3_4.txt')
+correct_json_data_3 = os.path.join(os.getcwd(), 'tests', 'fixtures', 'result_3_4.json')
+
+@pytest.mark.parametrize("data_1, data_2, format, result",
+                         [(data_3_json, data_4_json, "stylish", correct_stylish_data_3),
+                          (data_3_yml, data_4_yml, "stylish", correct_stylish_data_3),
+                          (data_3_json, data_4_json, "plane", correct_plane_data_3),
+                          (data_3_yml, data_4_yml, "plane", correct_plane_data_3),
+                          (data_3_yml, data_4_json, "stylish", correct_stylish_data_3),
+                          (data_3_json, data_4_yml, "stylish", correct_stylish_data_3),
+                          (data_3_yml, data_4_json, "plane", correct_plane_data_3),
+                          (data_3_json, data_4_yml, "plane", correct_plane_data_3)])
+def test_gendiff(data_1, data_2, format, result):
+    correct_result = open(result).read()
+    assert generate_diff(data_1, data_2, format) == correct_result
