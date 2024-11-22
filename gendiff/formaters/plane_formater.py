564230b1f16e91090b637b22437333036fc9d99f
Marvv1ne <gorn89@inbox.ru>
2024-11-22 22:21:51 +0400

    improve functions

--- gendiff/formaters/plane_formater.py
+++ gendiff/formaters/plane_formater.py
@@ -11,27 +11,31 @@ def plane_formater(tree, path=''):
         if value.get("type", None) == "leaf":            
             result.append(plane_leaf_formater(key, value, os.path.join(path, key)))                
         elif value.get("type", None) == "branch":            
-            result.extend(plane_formater(get_children(value), os.path.join(path, key)))        
+            result.extend(plane_formater(get_children(value), os.path.join(path, key)))      
     return ''.join(result)
 
 def plane_leaf_formater(key, leaf, path):
-    path = path.replace('\\', '.')
+    path = path.replace('/', '.')
     res = ''
     if leaf["status"] == "untouched":
         return ''
     elif leaf["status"] == "updated":
-        res = f"Property {path} was updated. "
-        res += f"From '{complex_formater(get_old_value(leaf))}' to "
-        res += f"'{complex_formater(get_new_value(leaf))}'\n"        
+        res = f"Property '{path}' was updated. "
+        res += f"From {type_formater(get_old_value(leaf))} to "
+        res += f"{type_formater(get_new_value(leaf))}\n"        
     elif leaf["status"] == "deleted":       
-        res += f"Property {path} was removed\n"
+        res += f"Property '{path}' was removed\n"
     elif leaf["status"] == "inserted":                
-        res += f"Property {path} was added with value: "
-        res += f"{complex_formater(get_new_value(leaf))}\n"
+        res += f"Property '{path}' was added with value: "
+        res += f"{type_formater(get_new_value(leaf))}\n"
     return res
     
 
-def complex_formater(value):
-    if not isinstance(value, dict):
-        return converter(value)
-    return "[complex value]"
+def type_formater(value):
+    value = converter(value)
+    if isinstance(value, dict):
+        return "[complex value]"
+    elif value in ("null", "true", "false"):
+        return value
+    elif isinstance(value, str):
+        return f"'{value}'"
