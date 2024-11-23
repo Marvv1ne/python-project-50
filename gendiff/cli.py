7e7e1481ca19b2e912022d5e87d8b4be6d29dfe7
Gura-san <gorn89@inbox.ru>
2024-11-23 12:18:12 +0400

    move cli from scripts folder up to gendiff folder

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,11 +1,11 @@
 import argparse
 
+
 def args_parser():
     parser = argparse.ArgumentParser(prog="gendiff",
-                                     usage="%(prog)s [-h] [-f FORMAT] fist_file, second_file",
-                                     description= "Compares two configuration files and shows a difference.",
+                                     description="Compares two configuration \
+                                     files and shows a difference.",
                                      )
-    
     parser.add_argument("-f", "--format",
                         help="set format of output",
                         default="stylish")
