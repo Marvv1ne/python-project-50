63d3bbb9f2dbc19e1566755c56dce5adbf18b8d0
Gura-san <gorn89@inbox.ru>
2024-11-23 12:21:54 +0400

    move generate_diff function from .gendiff/scripts/gendiff.py to ./gendiff/engin.py

---
+++ gendiff/engine.py
@@ -0,0 +1,18 @@
+from .tree import make_tree
+from .parser import file_reader
+from .formaters.stylish_formater import stylish_formater
+from .formaters.plane_formater import plane_formater
+from .formaters.json_formater import json_formater
+
+
+def generate_diff(first_file_name, second_file_name, format):
+    old_dict = file_reader(first_file_name)
+    new_dict = file_reader(second_file_name)
+    tree = make_tree(old_dict, new_dict)
+    match format:
+        case "stylish":
+            return stylish_formater(tree)
+        case "plane":
+            return plane_formater(tree).rstrip()
+        case "json":
+            return json_formater(tree)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,30 +1,11 @@
-from pprint import pprint
-from .cli import args_parser
-from .tree import make_tree
-from .parser import file_reader
-from pprint import pprint
-from ..formaters.stylish_formater import stylish_formater
-from ..formaters.plane_formater import plane_formater
-from ..formaters.json_formater import json_formater
+from ..cli import args_parser
+from ..engine import generate_diff
 
-
-def generate_diff(first_file_name, second_file_name, format):
-    old_dict = file_reader(first_file_name)
-    new_dict = file_reader(second_file_name)
-    tree = make_tree(old_dict, new_dict)
-    match format:
-        case "stylish":
-            return stylish_formater(tree)
-        case "plane":
-            return plane_formater(tree).rstrip()
-        case "json":
-            return json_formater(tree)
-    
 
 def main():
     first_file, second_file, format = args_parser()
-    print(generate_diff(first_file, second_file, format))   
-    
+    print(generate_diff(first_file, second_file, format))
+
 
 if __name__ == "__main__":
     main()
