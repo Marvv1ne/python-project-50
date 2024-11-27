85cdd70337388c877eea6a25a9889f7101cf8dd6
Gura-san <gorn89@inbox.ru>
2024-11-27 19:03:06 +0400

    improve generate_diff function with new functions

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,13 +1,18 @@
 from .tree import make_tree
-from .parser import file_reader
+from .file_reader import file_reader, get_data_format
+from .parser import parser
 from .formaters.stylish_formater import stylish_formater
 from .formaters.plane_formater import plane_formater
 from .formaters.json_formater import json_formater
 
 
 def generate_diff(first_file_name, second_file_name, format="stylish"):
-    old_dict = file_reader(first_file_name)
-    new_dict = file_reader(second_file_name)
+    old_data = file_reader(first_file_name)
+    new_data = file_reader(second_file_name)
+    old_data_format = get_data_format(first_file_name)
+    new_data_format = get_data_format(second_file_name)
+    old_dict = parser(old_data, old_data_format)
+    new_dict = parser(new_data, new_data_format)
     tree = make_tree(old_dict, new_dict)
     match format:
         case "stylish":
